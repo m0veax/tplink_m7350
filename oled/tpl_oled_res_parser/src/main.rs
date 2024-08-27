9e9371b8e206df2126e1c08e30e08d6c50d05a9e
Daniel Maslowski <info@orangecms.org>
2024-08-28 00:33:09 +0200

    oled/parser: dump sprites files to directory

--- oled/tpl_oled_res_parser/src/main.rs
+++ oled/tpl_oled_res_parser/src/main.rs
@@ -1,8 +1,6 @@
 use clap::Parser;
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
-use std::io::SeekFrom;
+use std::fs::{self, File};
+use std::io::{self, prelude::*, SeekFrom};
 use zerocopy::FromBytes;
 use zerocopy_derive::{AsBytes, FromBytes, FromZeroes};
 
@@ -32,6 +30,10 @@ struct Args {
     /// File to read
     #[arg(index = 1)]
     file: String,
+
+    // output directory
+    #[arg(index = 2, default_value = "./res")]
+    dest: String,
 }
 
 const META_SIZE: usize = 28;
@@ -39,6 +41,7 @@ const META_SIZE: usize = 28;
 fn main() -> io::Result<()> {
     let args = Args::parse();
     let file = args.file;
+    let dest = args.dest;
 
     println!("Scanning {file} for entries");
 
@@ -54,6 +57,9 @@ fn main() -> io::Result<()> {
 
     let mut pos = 2;
     let mut count = 0;
+
+    fs::create_dir_all(&dest).unwrap();
+
     for _ in 0..entries {
         f.seek(SeekFrom::Start(pos))?;
         let buf = &mut [0u8; META_SIZE];
@@ -67,10 +73,27 @@ fn main() -> io::Result<()> {
 
         let sm = SpriteMeta::read_from_prefix(buf).unwrap();
         let id = sm.sprite_id;
-        let len = ((sm.width * sm.height) as f64 / 8.0).ceil();
-        print!("{id} {len}; ");
+        let len = ((sm.width * sm.height) as f64 / 8.0).ceil() as usize;
 
-        pos += (META_SIZE + len as usize) as u64;
+        let mut buf = vec![0; len];
+        let buf = buf.as_mut_slice();
+        f.read_exact(buf).unwrap();
+
+        let width = sm.width;
+        let height = sm.height;
+        let mut filename = format!("{id}_{width}_{height}");
+        filename.push_str(".res");
+
+        let path = std::path::Path::new(&dest).join(filename);
+        let mut output = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(path)
+            .unwrap();
+        output.write_all(buf).unwrap();
+
+        pos += (META_SIZE + len) as u64;
         count += 1;
     }
 
